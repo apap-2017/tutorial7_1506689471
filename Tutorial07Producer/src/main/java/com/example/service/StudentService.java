b56a041002be0fe58451874347670bfb7d31dbf9
gabriellenatasha <gabriellenatasha407@gmail.com>
2017-11-03 14:36:54 +0700

    latihan 1, latihan 2

---
+++ Tutorial07Producer/src/main/java/com/example/service/StudentService.java
@@ -0,0 +1,25 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.example.model.CourseModel;
+import com.example.model.StudentModel;
+
+public interface StudentService
+{
+    StudentModel selectStudent (String npm);
+
+    List<StudentModel> selectAllStudents ();
+
+    void addStudent (StudentModel student);
+
+    void deleteStudent (String npm);
+    
+    void updateStudent(StudentModel student);
+    
+    CourseModel selectCourse(String idCourse);
+    
+    List<CourseModel> selectAllCourses();
+}
